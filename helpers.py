b84c7b6d08b54086bc47217e698ac2b6a06d226f
Lauren Shrack <laurenshrack@gmail.com>
2024-12-07 18:05:22 -0500

    added helpers with evaluate, get_calib_dataset, get_calib_feat

---
+++ helpers.py
@@ -0,0 +1,90 @@
+from functools import partial
+import torch
+from torch import nn
+import tqdm
+from datasets import load_dataset
+
+# evaluate function borrowed from Lab 2
+@torch.no_grad()
+def evaluate(model, tokenizer):
+    testenc = load_dataset("wikitext", "wikitext-2-raw-v1", split="test")
+    testenc = tokenizer("\n\n".join(testenc["text"]), return_tensors="pt")
+    testenc = testenc.input_ids.to(model.device)
+    nsamples = 40
+    model = model.eval()
+    nlls = []
+    for i in tqdm.tqdm(range(nsamples), desc="evaluating..."):
+        batch = testenc[:, (i * 2048) : ((i + 1) * 2048)].to(model.device)
+        with torch.no_grad():
+            lm_logits = model(batch).logits
+        shift_logits = lm_logits[:, :-1, :].contiguous().float()
+        shift_labels = testenc[:, (i * 2048) : ((i + 1) * 2048)][:, 1:]
+        loss_fct = nn.CrossEntropyLoss()
+        loss = loss_fct(
+            shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1)
+        )
+        neg_log_likelihood = loss.float() * 2048
+        nlls.append(neg_log_likelihood)
+    return torch.exp(torch.stack(nlls).sum() / (nsamples * 2048))
+
+
+def get_calib_dataset(tokenizer=None, n_samples=256, block_size=512):
+    dataset = load_dataset("mit-han-lab/pile-val-backup", split="validation")
+    dataset = dataset.shuffle(seed=42)
+    samples = []
+    n_run = 0
+    for data in dataset:
+        line = data["text"]
+        line = line.strip()
+        line_encoded = tokenizer.encode(line)
+        if len(line_encoded) > block_size:
+            continue
+        sample = torch.tensor([line_encoded])
+        if sample.numel() == 0:
+            continue
+        samples.append(sample)
+        n_run += 1
+        if n_run == n_samples:
+            break
+
+    # now concatenate all samples and split according to block size
+    cat_samples = torch.cat(samples, dim=1)
+    n_split = cat_samples.shape[1] // block_size
+    print(f" * Split into {n_split} blocks")
+    return [
+        cat_samples[:, i * block_size : (i + 1) * block_size] for i in range(n_split)
+    ]
+
+
+@torch.no_grad()
+def get_calib_feat(model, tokenizer):
+    input_dict = dict()
+
+    def stat_input_max_hook(m, x, y, name):
+        if isinstance(x, tuple):
+            x = x[0]
+        x_max = x.view(-1, x.shape[-1]).abs().mean(dim=0).cpu().detach()
+        if name not in input_dict:
+            input_dict[name] = [x_max]
+        else:
+            input_dict[name] += [x_max]
+
+    hooks = []
+    for name, m in model.named_modules():
+        if isinstance(m, nn.Linear):
+            hooks.append(
+                m.register_forward_hook(partial(stat_input_max_hook, name=name))
+            )
+
+    print("Collecting activation scales...")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    samples = get_calib_dataset(tokenizer)
+    pbar = tqdm.tqdm(samples)
+    for input_ids in pbar:
+        input_ids = input_ids.to(device)
+        model(input_ids)
+
+    for hook in hooks:
+        hook.remove()
+    return input_dict
